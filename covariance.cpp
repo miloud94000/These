42f6320aa725d5c22fb2fab22fd517d9b6778160
miloud94000 <miloudmezian@gmail.com>
2016-02-11 17:02:04 +0100

    Update covariance.cpp

--- covariance.cpp
+++ covariance.cpp
@@ -1,67 +1,52 @@
-#include "CovarianceMatrix.h"
+#include <iostream>
+#include <vector>
+#include <eigen3/Eigen/Dense>
+#include <cmath>
+#include <functional>
+#include <numeric>
+#include "ReaderFilePosPac/trajecto_reader.h"
 
-template <typename T> inline constexpr T carre(T const& v) { return v * v; }
 
-Covariance::Covariance(Trajecto &  trajecto) :
-          m_AccEvent(trajecto.AccuracySeries()),
-          m_NbreEvenement( m_AccEvent.Nevent() ),
-          m_covarianceX(Eigen::MatrixXd::Zero(m_NbreEvenement, m_NbreEvenement)),
-          m_covarianceY(Eigen::MatrixXd::Zero(m_NbreEvenement, m_NbreEvenement)),
-          m_covarianceZ(Eigen::MatrixXd::Zero(m_NbreEvenement, m_NbreEvenement))
+/*! La class CovarianceMatrix a pour but de calculer les matrices de covariances X,Y et Z à partir d'une trajectoire */
+
+class Covariance
 {
-    std::vector<double> v_normaliseX;
-    std::vector<double> v_normaliseY;
-    std::vector<double> v_normaliseZ;
 
-    assert(m_NbreEvenement > 0);
-    for(int k=0 ; k<(m_NbreEvenement-1) ; ++k){
 
-        auto const& ek1 = m_AccEvent.Event(k+1);
-        auto const& ek = m_AccEvent.Event(k);
+public:
 
-        const double dt=ek1.m_time - ek.m_time;
 
-        v_normaliseX.push_back(facteur_normalisation(ek.m_northPositionRMSError,ek1.m_northPositionRMSError,ek.m_northVelocityRMSError,dt));
-        v_normaliseY.push_back(facteur_normalisation(ek.m_eastPositionRMSError,ek1.m_eastPositionRMSError,ek.m_eastVelocityRMSError,dt));
-        v_normaliseZ.push_back(facteur_normalisation(ek.m_downPositionRMSError,ek1.m_downPositionRMSError,ek.m_downVelocityRMSError,dt));
+    Covariance(Trajecto & trajecto);
+
+
+     Eigen::MatrixXd const& getCovarianceX(){return m_covarianceX;}
+     Eigen::MatrixXd const& getCovarianceY(){return m_covarianceY;}
+     Eigen::MatrixXd const& getCovarianceZ(){return m_covarianceZ;}
+
+
+private :
+
+    /*! Objet Trajectoire contenant toutes les informations (Temps t, incertitudes positions et vitesses) pour calculer les matrices de covariances */
+
+
+
+    TEventSeries<AccuracyEvent> & m_AccEvent;
+    int m_NbreEvenement;
+    Eigen::MatrixXd  m_covarianceX;
+    Eigen::MatrixXd m_covarianceY;
+    Eigen::MatrixXd m_covarianceZ;
+
+
+
+    double facteur_normalisation(const double & sigma_p_i,const double & sigma_p_i_1,const double  & sigma_v_i,const double & dt){
+
+         return sqrt((sigma_p_i_1*sigma_p_i_1)/ ((dt*dt*sigma_v_i*sigma_v_i)+(sigma_p_i*sigma_p_i)) );
     }
 
-    for(int i=0 ; i<m_NbreEvenement; ++i){
-
-        auto const& event_i = m_AccEvent.Event(i);
-
-        const double cxi = carre(event_i.m_northPositionRMSError);
-        const double cyi = carre(event_i.m_eastPositionRMSError);
-        const double czi = carre(event_i.m_downPositionRMSError);
-
-        m_covarianceX(i,i) = cxi;
-        m_covarianceY(i,i) = cyi;
-        m_covarianceZ(i,i) = czi;
-
-        double normX =1.;
-        double normY = 1.;
-        double normZ= 1.;
-
-        for(int j=i+1 ; j<m_NbreEvenement; ++j){
-
-            normX *= v_normaliseX[j-1];
-            m_covarianceX(i,j) = cxi *normX;
-            m_covarianceX(j,i) = m_covarianceX(i,j);
-
-            normY *= v_normaliseY[j-1];
-            m_covarianceY(i,j) = cyi *normY ;
-            m_covarianceY(j,i) = m_covarianceY(i,j);
-
-            normZ*=v_normaliseZ[j-1];
-            m_covarianceZ(i,j) = czi *normZ  ;
-            m_covarianceZ(j,i)=m_covarianceZ(i,j);
-
-        }
-    }
-}
 
 
 
 
 
 
+};
