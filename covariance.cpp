f858cb79736761c93c1521c872ec34b153935fe7
miloud94000 <miloudmezian@gmail.com>
2016-02-11 17:01:50 +0100

    Update covariance.cpp

--- covariance.cpp
+++ covariance.cpp
@@ -1,81 +1,64 @@
 #include "CovarianceMatrix.h"
 
+template <typename T> inline constexpr T carre(T const& v) { return v * v; }
 
-void CovarianceMatrix::CovarianceMatrix(){
+Covariance::Covariance(Trajecto &  trajecto) :
+          m_AccEvent(trajecto.AccuracySeries()),
+          m_NbreEvenement( m_AccEvent.Nevent() ),
+          m_covarianceX(Eigen::MatrixXd::Zero(m_NbreEvenement, m_NbreEvenement)),
+          m_covarianceY(Eigen::MatrixXd::Zero(m_NbreEvenement, m_NbreEvenement)),
+          m_covarianceZ(Eigen::MatrixXd::Zero(m_NbreEvenement, m_NbreEvenement))
+{
+    std::vector<double> v_normaliseX;
+    std::vector<double> v_normaliseY;
+    std::vector<double> v_normaliseZ;
 
-    unsigned NbreEvenement =m_trajecto.AccuracySeries().Nevent(); // Indique le nombre d'évenement de la trajectoire
+    assert(m_NbreEvenement > 0);
+    for(int k=0 ; k<(m_NbreEvenement-1) ; ++k){
 
-    TEventSeries<AccuracyEvent> AccEvent=m_trajecto.AccuracySeries();
+        auto const& ek1 = m_AccEvent.Event(k+1);
+        auto const& ek = m_AccEvent.Event(k);
 
-    std::vector<double> v_normaliseX; /* Facteur de normalisation suivant X */
-    std::vector<double> v_normaliseY; /* Facteur de normalisation suivant Y */
-    std::vector<double> v_normaliseZ; /* Facteur de normalisation suivant Z */
+        const double dt=ek1.m_time - ek.m_time;
 
-    double dt;
-
-    for(unsigned i=0;i<(NbreEvenement-1);i++){
-
-        dt=AccEvent.Event(i+1).m_time-AccEvent.Event(i).m_time; // Différence de temps entre 2 évenements
-
-        /* Facteur de normalisation Ni suivant X */
-        v_normaliseX.push_back(facteur_normalisation(AccEvent.Event(i).m_northPositionRMSError,AccEvent.Event(i+1).m_northPositionRMSError,AccEvent.Event(i).m_northVelocityRMSError,dt));
-
-        /* Facteur de normalisation Ni suivant X */
-        v_normaliseY.push_back(facteur_normalisation(AccEvent.Event(i).m_eastPositionRMSError,AccEvent.Event(i+1).m_eastPositionRMSError,AccEvent.Event(i).m_eastVelocityRMSError,dt));
-
-        /* Facteur de normalisation Ni suivant Z */
-        v_normaliseZ.push_back(facteur_normalisation(AccEvent.Event(i).m_downPositionRMSError,AccEvent.Event(i+1).m_downPositionRMSError,AccEvent.Event(i).m_downVelocityRMSError,dt));
-
+        v_normaliseX.push_back(facteur_normalisation(ek.m_northPositionRMSError,ek1.m_northPositionRMSError,ek.m_northVelocityRMSError,dt));
+        v_normaliseY.push_back(facteur_normalisation(ek.m_eastPositionRMSError,ek1.m_eastPositionRMSError,ek.m_eastVelocityRMSError,dt));
+        v_normaliseZ.push_back(facteur_normalisation(ek.m_downPositionRMSError,ek1.m_downPositionRMSError,ek.m_downVelocityRMSError,dt));
     }
 
-    m_covarianceX=Eigen::MatrixXd::Zero(NbreEvenement, NbreEvenement); /* Matrice carré */
-    m_covarianceY=Eigen::MatrixXd::Zero(NbreEvenement, NbreEvenement); /* Matrice carré */
-    m_covarianceZ=Eigen::MatrixXd::Zero(NbreEvenement, NbreEvenement); /* Matrice carré */
+    for(int i=0 ; i<m_NbreEvenement; ++i){
 
+        auto const& event_i = m_AccEvent.Event(i);
 
-   double SommeNix,SommeNiy,SommeNiz ;
+        const double cxi = carre(event_i.m_northPositionRMSError);
+        const double cyi = carre(event_i.m_eastPositionRMSError);
+        const double czi = carre(event_i.m_downPositionRMSError);
 
-   for(unsigned i=0;i<NbreEvenement;i++){
+        m_covarianceX(i,i) = cxi;
+        m_covarianceY(i,i) = cyi;
+        m_covarianceZ(i,i) = czi;
 
-       for(unsigned j=i;j<NbreEvenement;j++){
+        double normX =1.;
+        double normY = 1.;
+        double normZ= 1.;
 
-           /*si i==j alors cov(Di,Dj)=(Sigma_position_i)²*/
-           m_covarianceX(i,j)=AccEvent.Event(i).m_northPositionRMSError*AccEvent.Event(i).m_northPositionRMSError; /*(Sigma_px_i)² */
-           m_covarianceY(i,j)=AccEvent.Event(i).m_eastPositionRMSError*AccEvent.Event(i).m_eastPositionRMSError;   /*(Sigma_py_i)² */
-           m_covarianceZ(i,j)=AccEvent.Event(i).m_downPositionRMSError*AccEvent.Event(i).m_downPositionRMSError;   /*(Sigma_pz_i)² */
+        for(int j=i+1 ; j<m_NbreEvenement; ++j){
 
-           if(i!=j){
+            normX *= v_normaliseX[j-1];
+            m_covarianceX(i,j) = cxi *normX;
+            m_covarianceX(j,i) = m_covarianceX(i,j);
 
-               /*si i!=j alors cov(Di,Dj)=(Sigma_position_i)² ∑ facteur de normalisation de i à j-1  */
+            normY *= v_normaliseY[j-1];
+            m_covarianceY(i,j) = cyi *normY ;
+            m_covarianceY(j,i) = m_covarianceY(i,j);
 
-               SommeNix=accumulate(v_normaliseX.begin()+i, v_normaliseX.begin()+j, 1., std::multiplies<double>());
-               m_covarianceX(i,j)=m_covarianceX(i,j)*SommeNix;
-               m_covarianceX(j,i)=m_covarianceX(i,j); /* Matrice symétrique */
+            normZ*=v_normaliseZ[j-1];
+            m_covarianceZ(i,j) = czi *normZ  ;
+            m_covarianceZ(j,i)=m_covarianceZ(i,j);
 
-               SommeNiy=accumulate(v_normaliseY.begin()+i, v_normaliseY.begin()+j, 1., std::multiplies<double>());
-               m_covarianceY(i,j)=m_covarianceY(i,j)*SommeNiy;
-               m_covarianceY(j,i)=m_covarianceY(i,j); /* Matrice symétrique */
-
-               SommeNiz=accumulate(v_normaliseZ.begin()+i, v_normaliseZ.begin()+j, 1., std::multiplies<double>());
-               m_covarianceZ(i,j)=m_covarianceZ(i,j)*SommeNiz;
-               m_covarianceZ(j,i)=m_covarianceZ(i,j); /* Matrice symétrique */
-
-           }
-
-
-       }
-
-
-
-   }
-
-
-
+        }
+    }
 }
-
-
-
-
 
 
 
